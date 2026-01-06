a4e555135f31034b746adf111e677e6d293b8486
viettt0119 <viettt0119@gmail.com>
2026-01-06 08:06:54 +0700

    test Console.Read

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,13 +1,8 @@
-﻿using System;
-
-class Program
-{
-    static void Main(string[] args)
+﻿internal class Program_Read{
+    private static void Main(String[] args)
     {
-        int a = 5;
-        int b = 10;
-
-        Console.WriteLine(a + b);
-        Console.WriteLine("test in ra man hinh ngay 6/1");
+        Console.WriteLine("Nhap du lieu tu ban phim:");
+        int x = Console.Read();
+        Console.WriteLine("du lieu vua nhap la "+ x);
     }
 }
--- NewApp/obj/Debug/net10.0/NewApp.AssemblyInfo.cs
+++ NewApp/obj/Debug/net10.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+815df548845510c60f4b946207b715f9712fe8e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+690cbf7d370fd1d46e876a907e4cb01d8146ca32")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
